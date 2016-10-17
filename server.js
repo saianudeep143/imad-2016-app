394a68f9a6734cb48f5f938eacca01e0df4c8160
saianudeep143 <saianudeep143@gmail.com>
2016-10-17 19:41:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
-  res.send('hiii ansi');
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
